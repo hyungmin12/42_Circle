61002867254f35805a4bc3d0e7e70c6b82651f2f
hmy <hmy940424@naver.com>
2022-07-13 00:47:11 +0900

    asdf

---
+++ ft_memcmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyyoo <hyyoo@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/12 23:53:35 by hyyoo             #+#    #+#             */
+/*   Updated: 2022/07/12 23:57:15 by hyyoo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*p_s1;
+	const unsigned char	*p_s2;
+
+	p_s1 = (const unsigned char *)s1;
+	p_s2 = (const unsigned char *)s2;
+	while (n--)
+	{
+		if (*p_s1 != *p_s2)
+			return (*p_s1 - *p_s2);
+		p_s1++;
+		p_s2++;
+	}
+	return (0);
+}
