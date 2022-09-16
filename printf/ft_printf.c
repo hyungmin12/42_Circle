7ab32eceb0d73717186bc988e36f675afbb1a560
Hyungmin Yoo <hyyoo@c2r6s2.42seoul.kr>
2022-09-16 23:03:55 +0900

    asdf

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,27 +6,28 @@
 /*   By: hyyoo <hyyoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 22:19:15 by hyyoo             #+#    #+#             */
-/*   Updated: 2022/09/08 16:57:05 by hyyoo            ###   ########.fr       */
+/*   Updated: 2022/09/16 19:53:04 by hyyoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
 static int	make_printf(const char c, va_list (*ap))
 {
-	int ret;
+	int	ret;
 
 	ret = 0;
 	if (c == 'c')
-		ret += (ft_putchar((char)va_arg((*ap), int)));//노션에 이유 정리// gcc 컴파일러라 그럼.
+		ret += (ft_putchar((char)va_arg((*ap), int)));
 	else if (c == 's')
 		ret += (ft_putstr(va_arg((*ap), char *)));
 	else if (c == 'd' || c == 'i')
 		ret += (ft_putnbr(va_arg((*ap), int)));
 	else if (c == 'u')
 		ret += (ft_putnbr(va_arg((*ap), unsigned int)));
-	else if (c == 'x' || c == 'X' || c == 'p')
+	else if (c == 'p')
+		ret += ft_putx(va_arg((*ap), unsigned long long));
+	else if (c == 'x' || c == 'X')
 		ret += (ft_puthex(va_arg((*ap), unsigned int), c));
 	else if (c == '%')
 	{
@@ -39,43 +40,23 @@ static int	make_printf(const char c, va_list (*ap))
 int	ft_printf(const char *str, ...)
 {
 	va_list	ap;
-	int	i;
-	int	count;
+	int		i;
+	int		count;
 
 	va_start(ap, str);
 	i = 0;
 	count = 0;
-	while(str[i])
+	while (str[i])
 	{
 		if (str[i] == '%' && str[i + 1] != '\0')
 		{	
-			i++;// %의 다음 즉 c,s,d 등을 표현
-			count = count + make_printf(str[i], &ap); // 현재 i는 c,s 등 여기서 가변인자 선언한 가변인자 ap를 make_print로 보냄
+			i++;
+			count = count + make_printf(str[i], &ap);
 		}
 		else
-		{
 			count = count + ft_putchar(str[i]);
-		}
 		i++;
 	}
 	va_end(ap);
 	return (count);
 }
-/*
-int main()
-{
-	
-	char a[10] = "";
-	char s = '\0';
-	int i = -2147483648;
-	int j = -1234;
-
-	int n = ft_printf("1 == %c, 2 == %s", s, a);
-	printf("\n");
-	ft_printf("n == %d", n);
-
-	ft_printf("\n");
-	int k = printf("1 == %c, 2 == %s", s, a);
-	printf("\n");
-	printf("k == %d", k);
-}*/
--- printf/ft_puthex.c
+++ printf/ft_puthex.c
@@ -6,20 +6,19 @@
 /*   By: hyyoo <hyyoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 17:24:16 by hyyoo             #+#    #+#             */
-/*   Updated: 2022/08/27 17:36:52 by hyyoo            ###   ########.fr       */
+/*   Updated: 2022/09/16 19:56:24 by hyyoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_puthex(unsigned long num, char c)
+int	ft_puthex(unsigned long num, char c)
 {
-	int	i;
+	int		i;
 	char	*hex;
 
 	i = 0;
 	hex = "0123456789abcdef";
-	
 	if (c == 'X')
 		hex = "0123456789ABCDEF";
 	if (num > 15)
@@ -31,22 +30,3 @@ int ft_puthex(unsigned long num, char c)
 		i += write (1, &hex[num], 1);
 	return (i);
 }
-/*
-int	ft_puthex(unsigned long n, char x)
-{
-	int		counter;
-	char	*str;
-
-	str = "0123456789abcdef";
-	counter = 0;
-	if (x == 'X')
-		str = "0123456789ABCDEF";
-	if (n > 15)
-	{
-		counter += ft_puthex(n / 16, x);
-		counter += ft_puthex(n % 16, x);
-	}
-	else
-		counter += ft_putchar(str[n]);
-	return (counter);
-}*/
