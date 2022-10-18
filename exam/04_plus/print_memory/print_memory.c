ac460058af557288c65b6258936892cbc15b9090
hyungmin12 <96573766+hyungmin12@users.noreply.github.com>
2022-10-18 22:53:06 +0900

    Update print_memory.c

--- exam/04_plus/print_memory/print_memory.c
+++ exam/04_plus/print_memory/print_memory.c
@@ -20,13 +20,14 @@ void	ft_print_hex(unsigned char c)
 	char *radix;
 
 	radix = "0123456789abcdef";
-	write(1, &radix[c / 16], 1);
+	write(1, &radix[c / 16], 1);		// C == 176, 앞자리 = "C / 16" , 뒤자리 = "C % 16"
 	write(1, &radix[c % 16], 1);
 }
 
 void	ft_print_char(unsigned char c)
 {
-
+	// 아스키코드에서 프린트가능한 범위
+	// 이 범위가 아니라면, .을 출력
 	if (c >= ' ' && c <= '~')
 		write(1, &c, 1);
 	else
@@ -35,39 +36,58 @@ void	ft_print_char(unsigned char c)
 
 void	print_memory(const void *addr, size_t size)
 {
+
+	// size가 size_t 이기떄문에
 	size_t i;
 	size_t j;
 
+	// 메모리는 한 바이트씩 표현한다.
 	unsigned char *ptr;
 
 	i = 0;
 	j = 0;
-	
+	// 캐스팅
 	ptr = (unsigned char*)addr;
+
+	// size는 sizeof()로 받은 크기이므로
+	// i는 한 바이트를 뜻한다.
+	// 즉 16바이트씩 계산 (맨 밑에 i += 16)
 	while (i < size)
 	{
+
+		// j = 0으로 초기화
 		j = 0;
+
+		// 16진수로 출력
 		while (j < 16 && i + j < size)
 		{
 			ft_print_hex(ptr[i + j]);
+			// 2바이트가 끝날때 마다, 띄어쓰기
 			if (j % 2)
 				write(1, " ", 1);
 			j++;
 		}
 		while (j < 16)
 		{
+			// i + j가 size만큼 채워졌을경우
+			// 그럼에도 출력값을 보면 알겠지만
+			// 세번째 줄에 4개의 0000 0000 0000 0000 이 있고 마지막 네칸이 비워져있음
+			// 그걸 위한것
 			write(1, "  ", 2);
 			if (j % 2)
 				write(1, " ", 1);
 			j++;
 		}
 
+		// char로 출력
 		j = 0;
 		while (j < 16 && i + j < size)
 		{
 			ft_print_char(ptr[i + j]);
 			j++;
 		}
+
+		// 개행후에, 16바이트 이동
 		write(1, "\n", 1);
 		i += 16;
 	}
