d84e32a561029404215b5e30c947c4f34efd246a
hmy <hmy940424@naver.com>
2022-10-18 15:11:49 +0900

    asdf

--- exam/list/ft_list_foreach/ft_list_foreach.c
+++ exam/list/ft_list_foreach/ft_list_foreach.c
@@ -1,5 +1,5 @@
 Expected files   : ft_list_foreach.c, ft_list.h
-Allowed functions: 
+Allowed functions:
 --------------------------------------------------------------------------------
 
 Write a function that takes a list and a function pointer, and applies this
@@ -21,3 +21,19 @@ typedef struct    s_list
     struct s_list *next;
     void          *data;
 }                 t_list;
+
+
+
+#include "ft_list.h"
+
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	t_list *list_ptr;
+
+	list_ptr = begin_list;
+	while (list_ptr)
+	{
+		(*f)(list_ptr->data);
+		list_ptr = list_ptr->next;
+	}
+}
--- exam/list/sort_list.c
+++ exam/list/sort_list.c
@@ -30,3 +30,25 @@ int ascending(int a, int b)
 {
 	return (a <= b);
 }
+
+---------------------------------------------------------------------------------
+
+#include "list.h"
+
+t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
+{
+	t_list *begin = lst; 
+	t_list *future; 
+	while(lst)
+	{
+		future = lst->next;
+		while (future)
+		{
+			if (!cmp(lst->data, future->data))
+				ft_swap(&lst->data, &future->data);
+			future = future->next;
+		}
+		lst = lst->next;
+	}
+	return (begin);
+}
