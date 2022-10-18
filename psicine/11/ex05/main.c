42ac1c50076d0d03ca2b8d1d69bb334722c702ed
hmy <hmy940424@naver.com>
2022-10-18 14:32:40 +0900

    assdf

--- psicine/11/ex05/main.c
+++ psicine/11/ex05/main.c
@@ -1,30 +1,18 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hyyoo <hyyoo@student.42seoul.kr>           +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/06 19:26:07 by hyyoo             #+#    #+#             */
-/*   Updated: 2022/10/17 14:38:06 by hyyoo            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
-#include "ft_head.h"
-
+#include "ft.h"
+ 
 int	main(int ac, char **av)
 {
-	long	result;
-
+	int	result;
+ 
 	if (ac == 4)
 	{
-		if (no_do_this(av[2]) == -1)
+		if (check_error(av[2]) == -1)
 		{
 			write(1, "0\n", 2);
 			return (0);
 		}
-		result = calcul(ft_atoi(av[1]), ft_atoi(av[3]), av[2][0]);
+		result = ft_cal(ft_atoi(av[1]), ft_atoi(av[3]), av[2][0]);
 		if (ft_atoi(av[3]) == 0 && av[2][0] == '/')
 		{
 			write(1, "Stop : division by zero\n", 24);
@@ -40,8 +28,8 @@ int	main(int ac, char **av)
 	}
 	return (0);
 }
-
-int	no_do_this(char *str)
+ 
+int	check_error(char *str)
 {
 	if (str[1] != 0)
 		return (-1);
@@ -50,30 +38,17 @@ int	no_do_this(char *str)
 		return (1);
 	return (-1);
 }
-
-int	calcul(long ch1, long ch2, char op)
+ 
+int	ft_cal(int num1, int num2, char op)
 {
 	if (op == '+')
-		return (add(ch1, ch2));
+		return (ft_plus(num1, num2));
 	else if (op == '-')
-		return (moin(ch1, ch2));
+		return (ft_minor(num1, num2));
 	else if (op == '*')
-		return (x_this(ch1, ch2));
-	else if (op == '/' && ch2 != 0)
-		return (div(ch1, ch2));
+		return (ft_multi(num1, num2));
+	else if (op == '/' && num2 != 0)
+		return (ft_div(num1, num2));
 	else
-		return (mod(ch1, ch2));
-}
-
-int	this_size(long nb)
-{
-	int	i;
-
-	i = 0;
-	while (nb)
-	{
-		nb /= 10;
-		i++;
-	}
-	return (i);
+		return (ft_mod(num1, num2));
 }
